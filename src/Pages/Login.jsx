0c609086d9828be31d6e4832d11cee5f02001fef
akd420 <ayanakd112@gmail.com>
2023-11-25 19:05:08 +0600

    added modal with role selection for new users in login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,17 +5,31 @@ import { TbFidgetSpinner } from "react-icons/tb";
 import useAuth from "../Hooks/useAuth";
 import useToast from "../Components/Shared/useToast";
 import { useState } from "react";
+import { axiosSecure } from "../Hooks/useAxios";
 
 const Login = () => {
-  const { signInUser, googleLogin } = useAuth();
+  const { signInUser, googleLogin, user } = useAuth();
   const [loading, setLoading] = useState(false);
   const [showRoleModal, setShowRoleModal] = useState(false);
-const [selectedRole, setSelectedRole] = useState(null);
-
+  const [selectedRole, setSelectedRole] = useState(null);
   const navigate = useNavigate();
   const location = useLocation();
   const toast = useToast();
   console.log(loading);
+
+  const handleRoleSelect = async (role) => {
+    setSelectedRole(role);
+    try {
+      await postDataToDatabase({
+        name: user?.displayName,
+        email: user?.email,
+        role: role,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    setShowRoleModal(false);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,16 +37,15 @@ const [selectedRole, setSelectedRole] = useState(null);
     const email = form.email.value;
     const password = form.password.value;
 
-
     setLoading(true);
     signInUser(email, password)
       .then(() => {
         navigate(location?.state ? location.state : "/");
-        toast.success({content: "Login Successful"});
+        toast.success({ content: "Login Successful" });
         setLoading(false);
       })
       .catch((error) => {
-        toast.error({content: error.message});
+        toast.error({ content: error.message });
         setLoading(false);
       });
   };
@@ -51,6 +64,44 @@ const [selectedRole, setSelectedRole] = useState(null);
   //       setLoading(false);
   //     });
   // };
+  const handleGoogleSignInUser = async () => {
+    setLoading(true);
+
+    try {
+      const result = await googleLogin();
+      const userExists = await checkUserExists(result.user?.email);
+
+      if (userExists) {
+        navigate(location?.state ? location.state : "/");
+        toast.success({ content: "Login Successful" });
+      } else {
+        setShowRoleModal(true);
+        toast.success({ content: "Login Successful" });
+      }
+    } catch (error) {
+      toast.error({ content: error.message });
+    } finally {
+      setLoading(false);
+    }
+  };
+  const postDataToDatabase = async (data) => {
+    try {
+      await axiosSecure.post("users", data);
+      navigate(location?.state ? location.state : "/");
+      toast.success({ content: "User Saved to Database" });
+    } catch (error) {
+      toast.error({ content: "Error sending data to the database" });
+    }
+  };
+  const checkUserExists = async (email) => {
+    try {
+      const response = await axiosSecure.get(`users?email=${email}`);
+      return response.data.length > 0;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error checking user existence");
+    }
+  };
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
@@ -110,6 +161,68 @@ const [selectedRole, setSelectedRole] = useState(null);
             </button>
           </div>
         </form>
+        {/* modal section */}
+        {showRoleModal && (
+          <div>
+            {/* Put this part before </body> tag */}
+            <input
+              type="checkbox"
+              id="my_modal_6"
+              className="modal-toggle"
+              defaultChecked={showRoleModal}
+            />
+            <div
+              className={`modal ${showRoleModal ? "open" : ""}`}
+              role="dialog"
+              onClick={() => setShowRoleModal(false)}
+            >
+              <div className="modal-box">
+                <h3 className="font-bold text-lg text-center">
+                  Select Your <span className="text-rose">Role</span>
+                </h3>
+                <div className="mt-5">
+                  <div className="flex flex-wrap gap-6 justify-center items-center">
+                    <div
+                      className="flex flex-col items-center justify-center m-4"
+                      onClick={() => handleRoleSelect("organizer")}
+                    >
+                      <img src="/Organizer.png" alt="" className="w-20 h-20" />
+                      <p className="text-center font-semibold mt-3">
+                        Organizer
+                      </p>
+                    </div>
+                    <div
+                      className="flex flex-col items-center justify-center m-4"
+                      onClick={() => handleRoleSelect("professional")}
+                    >
+                      <img
+                        src="/Professional.png"
+                        alt=""
+                        className="w-20 h-20"
+                      />
+                      <p className="text-center font-semibold mt-3">
+                        Professional
+                      </p>
+                    </div>
+                    <div
+                      className="flex flex-col items-center justify-center m-4"
+                      onClick={() => handleRoleSelect("participant")}
+                    >
+                      <img
+                        src="/Participant.png"
+                        alt=""
+                        className="w-20 h-20"
+                      />
+                      <p className="text-center font-semibold mt-3">
+                        Participant
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         <div className="flex items-center pt-4 space-x-1">
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
           <p className="px-3 text-sm dark:text-gray-400">
