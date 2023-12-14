b9c8330f9f79ea4030979e5b01f5f07199db8a58
akd420 <ayanakd112@gmail.com>
2023-12-15 01:35:09 +0600

    fixed the map marker icon

--- src/Pages/ContactUs/Map.jsx
+++ src/Pages/ContactUs/Map.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
+import L from "leaflet"
 const Map = ({ position, popUp }) => {
+  const icon = L.icon({ iconUrl: "/marker-icon.png" });
   return (
     <MapContainer
       // className="mx-auto z-10"
@@ -21,7 +22,7 @@ const Map = ({ position, popUp }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker icon={icon} position={position}>
         <Popup>{popUp}.</Popup>
       </Marker>
     </MapContainer>
